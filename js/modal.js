b45e73d97cfea1aa2bd9fdef7c4515da36955188
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-05-10 00:30:50 +0200

    feat: card animation - test

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,16 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+// function myFunction(x) {
+// x.classList.toggle("change");
+// }
+
+// Get the button, and when the user clicks on it, execute myFunction
+// document.getElementById("myBtn").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+// function myFunction() {
+  // document.getElementById("myDropdown").classList.toggle("show");
+  // x.classList.toggle("show");
+// }
